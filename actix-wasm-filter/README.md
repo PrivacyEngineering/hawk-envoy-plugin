7a2fda9f74e0bc00fbfebbafa592c821b573fbd8
Juan Goyes <juan.goyes@car-ewallet.de>
2022-01-17 00:31:40 +0100

    Actix filter
    - Update documentation
    - Include references from envoy documentation and web assembly proxy
    - Pre-Implementation of interceptors for headers and body (request and response) at http layer 7

--- actix-wasm-filter/README.md
+++ actix-wasm-filter/README.md
@@ -1,12 +1,16 @@
 # actix-wasm-filter
 
 istio/envoy extension using WASM for Rust to intercept request and response traffic for
-services using istio ingress
+services using istio ingress. HTTP (L7) extensions.
 
 Based on:
 
 - [Extending envoy with WASM and Rust](https://antweiss.com/blog/extending-envoy-with-wasm-and-rust/)
 - [Proxy WASM Rust](https://github.com/otomato-gh/proxy-wasm-rust)
+- [Proxy-Wasm vNEXT ABI (Application Binary Interface) specification](https://github.com/proxy-wasm/spec/tree/master/abi-versions/vNEXT)
+- [Envoy Proxy: Life of a request](https://www.envoyproxy.io/docs/envoy/latest/intro/life_of_a_request#request-flow)
+- [Envoy Proxy: Tracing Request ID generation](https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/observability/tracing#arch-overview-tracing)
+- [Envoy Proxy: x-request-id header](https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_conn_man/headers#config-http-conn-man-headers-x-request-id)
 
 ## Getting started
 
@@ -51,28 +55,49 @@ docker-compose -f ./release/docker-compose.yaml rm
 ## Istio example
 
 It is required to use istio gateway for the traffic because the http filter is applied for
-gateway. It is possible to apply it for inbound or outbound proxy traffic (envoy) but it should go 
+gateway. It is possible to apply it for inbound or outbound proxy traffic (envoy) but it should go
 through the gateway for the filter to work.
 
 1. Run the Google cloud setup with istio enable
-2. Install httpbin
+2. Create namespace for httpbin demo project
 
 ```shell
-kaf https://raw.githubusercontent.com/istio/istio/release-1.12/samples/httpbin/httpbin.yaml
+kaf release/istio/httbin.gateway.ns.yaml
 ```
 
-3. Configure gateway
-4. Run release/istio/ files to install the filter. Istio will install the filter in each envoy proxy
-5. Export variables to access istio ingress
+3. Install httpbin
+
+```shell
+kaf https://raw.githubusercontent.com/istio/istio/release-1.12/samples/httpbin/httpbin.yaml -n httpbin-gateway
+```
+
+4. Configure istio gateway for httpbin project
+
+```shell
+kaf release/istio/istio.gateway.httpbin.yaml
+```
+
+5. Run release/istio/ files to install the filter. Istio will install the filter in each envoy proxy
+6. Export variables to access istio ingress
 
 ```shell
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
-export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
-export TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')
 ```
 
-6. Execute the test
+7. Test the isitio ingress gateway before install the filter. It should get 200 OK
+
+```shell
+curl -v -s -I "http://$INGRESS_HOST:$INGRESS_PORT/headers"
+```
+
+8. Install prime filter
+
+```shell
+kaf release/istio/filter
+```
+
+9. Execute the test
 
 HTTP/1.1 403 Forbidden
 ```shell
@@ -84,6 +109,11 @@ HTTP/1.1 200 OK
 curl -H "x-prime-token":"32323" -v -s -I "http://$INGRESS_HOST:$INGRESS_PORT/headers"
 ```
 
+10. Delete prime filter
+
+```shell
+k delete -f release/istio/filter
+```
 
 ## Useful commands
 
--- actix-wasm-filter/src/lib.rs
+++ actix-wasm-filter/src/lib.rs
@@ -1,42 +1,88 @@
 use log::{info, trace};
 use primes::is_prime;
 use proxy_wasm::traits::{Context, HttpContext};
-use proxy_wasm::types::{Action, LogLevel};
+use proxy_wasm::types::{Action, Bytes, LogLevel};
 
 #[no_mangle]
 pub fn _start() {
     proxy_wasm::set_log_level(LogLevel::Trace);
-    proxy_wasm::set_http_context(|context_id, _| -> Box<dyn HttpContext> {
-        Box::new(PrimeAuthorizer { context_id })
+    proxy_wasm::set_http_context(|context_id, root_context_id| -> Box<dyn HttpContext> {
+        Box::new(ActixFilter { context_id, root_context_id })
     });
 }
 
-struct PrimeAuthorizer {
+struct ActixFilter {
     context_id: u32,
+    root_context_id: u32,
 }
 
-impl Context for PrimeAuthorizer {}
+impl Context for ActixFilter {
+}
 
-impl HttpContext for PrimeAuthorizer {
-    fn on_http_request_headers(&mut self, _: usize) -> Action {
-        info!("PrimeAuthorizer started. #{}", self.context_id);
+impl HttpContext for ActixFilter {
 
-        match self.get_http_request_header("x-prime-token") {
-            Some(token) if token.parse::<u64>().is_ok() && is_prime(token.parse().unwrap()) => {
-                info!("PrimeAuthorizer. OK. #{} -> Header x-prime-token: {}", self.context_id, token);
-                self.resume_http_request();
-                Action::Continue
-            }
-            _ => {
-                info!("PrimeAuthorizer. #{} -> Access forbidden", self.context_id);
-                self.send_http_response(
-                    403,
-                    vec![("Powered-By", "proxy-wasm")],
-                    Some(b"Access forbidden.\n"),
-                );
-                Action::Pause
-            }
-        }
+    /// Called when HTTP request headers are received from the client.
+    /// Headers can be retrieved using proxy_get_map and/or proxy_get_map_value.
+    ///
+    /// # Arguments
+    /// * i32 (uint32_t) context_id
+    /// * i32 (size_t) num_headers
+    /// * i32 (bool) end_of_stream
+    ///
+    /// # Return
+    /// * i32 (proxy_action_t) next_action
+    fn on_http_request_headers(&mut self, _num_headers: usize) -> Action {
+        info!("ActixFilter started. #{}", self.context_id);
+
+        // let headers = self.get_http_request_headers(&self);
+        // info!("ActixFilter headers. #{}", headers);
+
+        return Action::Continue
     }
+
+    /// Called for each chunk of HTTP request body received from the client.
+    /// Request body can be retrieved using proxy_get_buffer.
+    ///
+    /// # Arguments
+    /// * i32 (uint32_t) context_id
+    /// * i32 (size_t) body_size
+    /// * i32 (bool) end_of_stream
+    ///
+    /// # Return
+    /// * i32 (proxy_action_t) next_action
+    fn on_http_request_body(&mut self, _body_size: usize, _end_of_stream: bool) -> Action {
+        // self.get_http_request_body(start, max);
+        todo!()
+    }
+
+    /// Called when HTTP response headers are received from the upstream.
+    /// Headers can be retrieved using proxy_get_map and/or proxy_get_map_value.
+    ///
+    /// # Arguments
+    /// * i32 (uint32_t) context_id
+    /// * i32 (size_t) num_headers
+    /// * i32 (bool) end_of_stream
+    ///
+    /// # Return
+    /// * i32 (proxy_action_t) next_action
+    fn on_http_response_headers(&mut self, _num_headers: usize) -> Action {
+        // let headers = self.get_http_response_headers(&self);
+        todo!()
+    }
+
+    /// Called for each chunk of HTTP response body received from the client.
+    /// Response body can be retrieved using proxy_get_buffer.
+    ///
+    /// # Arguments
+    /// * i32 (uint32_t) context_id
+    /// * i32 (size_t) body_size
+    /// * i32 (bool) end_of_stream
+    ///
+    /// # Return
+    /// * i32 (proxy_action_t) next_action
+    fn on_http_response_body(&mut self, _body_size: usize, _end_of_stream: bool) -> Action {
+        todo!()
+    }
+
 }
 
