38c8b2a26278564bc674fdf3987de4aa939798e6
Juan Goyes <juan.goyes@car-ewallet.de>
2022-02-03 00:31:56 +0100

    Update collector
    Get messages from istio plugin
    write to rabbitmq as is

--- collector/go.mod
+++ collector/go.mod
@@ -1,3 +1,7 @@
 module github.com/TUB-CNPE-TB/rust-envoy-proxy/collector
 
 go 1.17
+
+require (
+	github.com/streadway/amqp v1.0.0
+)
--- collector/main.go
+++ collector/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"context"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+const (
+	routingKey  = "queue.collector"
+	rabbitMqUrl = "amqp://guest:guest@rabbitmq-service:5672/"
+)
+
+var (
+	lines = make(chan message)
+)
+
 func main() {
+	ctx, done := context.WithCancel(context.Background())
+	go func() {
+		publish(redial(ctx, rabbitMqUrl), lines)
+	}()
+
 	http.HandleFunc("/", rootHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println(http.ListenAndServe(":8080", nil))
+	done()
 }
 
 func rootHandler(_ http.ResponseWriter, request *http.Request) {
@@ -36,5 +52,5 @@ func rootHandler(_ http.ResponseWriter, request *http.Request) {
 	}
 
 	log.Printf("BODY: %v", string(bodyBytes))
-
+	lines <- bodyBytes
 }
